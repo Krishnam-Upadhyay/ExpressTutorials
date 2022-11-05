2eddcd3f496ac4db4f8bb32239a4bfdefd701cb8
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2022-11-05 11:42:52 +0530

    Cutie Cat Learned has learned Post Methods in Express

---
+++ Post Method/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+//
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+const gorceryList = [{
+    item:'milk',
+    quantity:2,
+},
+{
+item:'cereal',
+quantity:1,
+},
+{
+item:'potato',
+quantity:3,
+}]
+
+app.listen(PORT,()=>console.log('server satarted on port number '));
+app.get('/groceries',(req,res)=>{
+    res.send(gorceryList)
+
+
+
+})
+
+app.post('/groceries',(req,res)=>{
+    console.log(req.body);
+    gorceryList.push(req.body);
+    res.sendStatus(201);
+
+}
+)
